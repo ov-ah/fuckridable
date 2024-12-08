e538168635b80e797f32de03b0d56ca31fda0537
GitHub Action <action@github.com>
2024-12-08 06:38:24 +0000

    Initialize template repository proper
    Set package name to com.github.ov-ah.fuckridable
    Set project name to fuckridable
    Set modid to fuckridable

--- src/main/java/com/github/ov-ah/fuckridable/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/ov-ah/fuckridable/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.ov-ah.fuckridable.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
